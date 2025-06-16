200a4703f3a5fd5a069665549230920763c9844f
anjalisoni210703 <sonianjali2310@gmail.com>
2025-06-16 16:35:11 +0530

    update

--- Abroad/src/main/java/com/abroad/security/JwtAuthFilter.java
+++ Abroad/src/main/java/com/abroad/security/JwtAuthFilter.java
@@ -1,33 +1,17 @@
 package com.abroad.security;
 
-import com.abroad.repository.SuperAdminRepository;
 import com.abroad.service.JwtService;
-import com.abroad.service.SuperAdminService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.abroad.entity.SuperAdmin;
-import com.abroad.repository.SuperAdminRepository;
 
 import java.io.IOException;
 
