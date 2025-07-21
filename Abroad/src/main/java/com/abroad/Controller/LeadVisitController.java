74c839444499719c1a9536e3c03e5d201892972b
DevSamruddhi <samruddhidhamale18@gmail.com>
2025-07-21 17:37:23 +0530

    LeadVisit

--- Abroad/src/main/java/com/abroad/Controller/LeadVisitController.java
+++ Abroad/src/main/java/com/abroad/Controller/LeadVisitController.java
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://wayabroad.in")
 public class LeadVisitController {
 
     @Autowired
     private LeadVisitServiceImpl leadVisitService;
 
-    @PostMapping("addLeadVisit/{lead_id}")
+    @PostMapping("/addLeadVisit/{lead_id}")
     public ResponseEntity<LeadVisit> addVisit(@PathVariable Long lead_id,
                                               @RequestParam String role,
                                               @RequestParam String email,
@@ -26,25 +27,25 @@ public class LeadVisitController {
         return ResponseEntity.ok(leadVisitService.addVisit(lead_id,role,email ,remark, visitCount, status));
     }
 
-    @GetMapping("getAllVisits")
+    @GetMapping("/getAllVisits")
     public List<LeadVisit> getAllVisits(@RequestParam String role,
                                         @RequestParam String email){
         return leadVisitService.getAllVisits(role,email);
     }
 
-    @GetMapping("getVisitById/{id}")
+    @GetMapping("/getVisitById/{id}")
     public ResponseEntity<LeadVisit> getVisitById(@PathVariable Long id,
                                                   @RequestParam String role,
                                                   @RequestParam String email){
         return ResponseEntity.ok(leadVisitService.getVisitById(id,role,email));
     }
-    @DeleteMapping("deleteVisit/{id}")
+    @DeleteMapping("/deleteVisit/{id}")
     public ResponseEntity<?> deleteVisit(@PathVariable Long id,
                                          @RequestParam String role,
                                          @RequestParam String email){
         return ResponseEntity.ok(leadVisitService.deleteVisit(id,role,email));
     }
-    @GetMapping("getVisitByLeadId/{id}")
+    @GetMapping("/getVisitByLeadId/{id}")
     public ResponseEntity<?> getLeadById(@PathVariable Long id,
                                                   @RequestParam String role,
                                                   @RequestParam String email){
