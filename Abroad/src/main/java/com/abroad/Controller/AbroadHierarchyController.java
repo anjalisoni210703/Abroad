d1d656456322de0ae03345a26b433bcdabd91586
DevSamruddhi <samruddhidhamale18@gmail.com>
2025-07-24 17:59:25 +0530

    Updated Enquiry

--- Abroad/src/main/java/com/abroad/Controller/AbroadHierarchyController.java
+++ Abroad/src/main/java/com/abroad/Controller/AbroadHierarchyController.java
@@ -4,12 +4,14 @@ import com.abroad.DTO.AbroadContinentDTO;
 import com.abroad.Service.AbroadHierarchyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "https://wayabroad.in")
 public class AbroadHierarchyController {
 
     private final AbroadHierarchyService hierarchyService;
