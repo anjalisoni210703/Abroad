511cd7afb561795a26915aac72d69c963cd76448
DevSamruddhi <samruddhidhamale18@gmail.com>
2025-08-14 15:07:53 +0530

    ContactUs

--- Abroad/src/main/java/com/abroad/JWT/SecurityConfig.java
+++ Abroad/src/main/java/com/abroad/JWT/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                     return config;
                 }))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/stafflogin","/userLogin","/permissionForUser","/getBranchCodeByUserEmail","/getAllBlogs","/getBlogByTitle","/createPartner","/createExamPreparation","/getExamById/{id}","/getAllExam","/getAllCourseName","/getAllStreams").permitAll()
+                        .requestMatchers("/stafflogin","/userLogin","/permissionForUser","/getBranchCodeByUserEmail","/getAllBlogs","/getBlogByTitle","/createPartner","/createExamPreparation","/getExamById/{id}","/getAllExam","/getAllCourseName","/getAllStreams","/createRegisterForm","/createContactUS","/createAboutUs").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
